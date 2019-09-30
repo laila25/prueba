4616ab31b538010032228a2700a334e47e1fba18
laila25 <laila2519@gmail.com>
2019-09-30 14:19:44 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import OrgChart from "react-orgchart";
+import "react-orgchart/index.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      url: "",
+      children: []
+    };
+    this.getData = this.getData.bind(this);
+    this.getData();
+  }
+
+  getData() {
+    fetch("./data.json")
+      .then(response => response.json())
+      .then(data => {
+        console.log(data[0].name);
+        this.setState({
+          name: data[0].name
+        });
+        return fetch(`./data-children${data[0].id}.json`)
+          .then(response => response.json())
+          .then(data => {
+            console.log(data);
+            this.setState({
+              children: data
+            });
+          });
+      });
+    fetch("./data.json")
+      .then(response => response.json())
+      .then(data => {
+        return fetch(`./data-img${data[0].id}.json`)
+          .then(response => response.json())
+          .then(data => {
+            this.setState({
+              url: data.url
+            });
+          });
+      });
+  }
+
+  render() {
+    const MyNodeComponent = ({ node }) => {
+      return (
+        <div className="initechNode">
+          <img src={node.url} className="img"></img>
+          <p>{node.name}</p>
+        </div>
+      );
+    };
+
+    console.log(this.state);
+    return (
+      <div className="App" id="initechOrgChart">
+        <OrgChart tree={this.state} NodeComponent={MyNodeComponent} />
+      </div>
+    );
+  }
 }
 
 export default App;
