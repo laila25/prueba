8073b93e4aaa6b99c3dba1fdcd402a7c60332424
laila25 <laila2519@gmail.com>
2019-10-01 17:20:41 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends React.Component {
     this.state = {
       nombre_empleado: "",
       id: "",
-      url: "",
+      foto_empleado: "",
       children: [],
       parent: []
     };
@@ -19,39 +19,42 @@ class App extends React.Component {
   }
 
   getData(id) {
-    fetch(`https://adalab-whoiswho.azurewebsites.net/api/employees/${id}`)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        this.setState({
-          nombre_empleado: data.nombre_empleado + data.apellidos_empleado,
-          id: data.id_empleado,
-          url: data.foto_empleado
-        });
-        return fetch(
-          `https://adalab-whoiswho.azurewebsites.net/api/employees/${data.id_superior}`
-        )
-          .then(response => response.json())
-          .then(data => {
-            console.log(data);
-            this.setState({
-              parent: [data]
-            });
-            if (data.id_superior !== "") {
-              return fetch(
-                `https://adalab-whoiswho.azurewebsites.net/api/employees/${data.id_superior}`
-              )
-                .then(response => response.json())
-                .then(data => {
-                  console.log(data);
-                  const spread = [data, ...this.state.parent];
-                  this.setState({
-                    parent: spread
-                  });
-                });
-            }
+    console.log(id);
+    if (!isNaN(id)) {
+      fetch(`https://adalab-whoiswho.azurewebsites.net/api/employees/${id}`)
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+          this.setState({
+            nombre_empleado: data.nombre_empleado + data.apellidos_empleado,
+            id: data.id_empleado,
+            foto_empleado: data.foto_empleado
           });
-      });
+          return fetch(
+            `https://adalab-whoiswho.azurewebsites.net/api/employees/${data.id_superior}`
+          )
+            .then(response => response.json())
+            .then(data => {
+              console.log(data);
+              this.setState({
+                parent: [data]
+              });
+              if (data.id_superior !== "") {
+                return fetch(
+                  `https://adalab-whoiswho.azurewebsites.net/api/employees/${data.id_superior}`
+                )
+                  .then(response => response.json())
+                  .then(data => {
+                    console.log(data);
+                    const spread = [data, ...this.state.parent];
+                    this.setState({
+                      parent: spread
+                    });
+                  });
+              }
+            });
+        });
+    }
   }
 
   getValue(ev) {
@@ -68,7 +71,7 @@ class App extends React.Component {
     const MyNodeComponent = ({ node }) => {
       return (
         <div className="initechNode" onClick={this.consolea} data-id={node.id}>
-          <img src={node.url} className="img"></img>
+          <img src={node.foto_empleado} className="img"></img>
           <p>{node.nombre_empleado}</p>
         </div>
       );
